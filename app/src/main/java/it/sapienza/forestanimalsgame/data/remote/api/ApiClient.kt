de101a664e91eeaf8435924c6217b63c9394f196
Volpeffervescente <martinafortuna2002@gmail.com>
2026-01-26 19:05:28 +0100

    new files added

--- app/src/main/java/it/sapienza/forestanimalsgame/data/remote/api/ApiClient.kt
+++ app/src/main/java/it/sapienza/forestanimalsgame/data/remote/api/ApiClient.kt
@@ -2,27 +2,22 @@ package it.sapienza.forestanimalsgame.data.remote.api
 
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
     // Esempio: "https://forestanimal-api-xxxxx-ew.a.run.app/"
     private const val BASE_URL = "https://forestanimal-api-1002662831596.europe-west12.run.app/"
 
-    private val okHttp by lazy {
-        OkHttpClient.Builder()
-            .connectTimeout(20, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS)
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()) // ✅ QUESTO
             .build()
     }
 
     val profileApi: ProfileApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttp)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-            .create(ProfileApi::class.java)
+        retrofit.create(ProfileApi::class.java)
     }
+
 }
